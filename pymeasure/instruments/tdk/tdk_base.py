746329d419a91d2e693960764493d8dd3d1cf527
manno005 <manno005@umn.edu>
2023-04-21 13:41:33 -0500

    Update tdk_base.py
    Finished updating tdk_base. Updated docstrings and changed a few validators. Need a reminder on what the starting ask and write methods are doing. Are they removing an auto replied "OK\r" command?

--- pymeasure/instruments/tdk/tdk_base.py
+++ pymeasure/instruments/tdk/tdk_base.py
@@ -22,36 +22,56 @@
 # THE SOFTWARE.
 #
 
-import logging
-from time import sleep
-
-import numpy as np
+# =============================================================================
+# Libraries / modules
+# =============================================================================
 
 from pymeasure.instruments import Instrument
-from pymeasure.instruments.validators import strict_discrete_set, strict_discrete_range
+from pymeasure.instruments.validators import strict_range
+from pymeasure.instruments.validators import strict_discrete_set
+from pymeasure.instruments.validators import strict_discrete_range
+import logging
+from time import sleep
+import numpy as np
+
+# =============================================================================
+# Logging
+# =============================================================================
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+# =============================================================================
+# Instrument file
+# =============================================================================
+
 
 class TDK_Lambda_Base(Instrument):
     """
-    This is the base class for TDK Lambda Genesys
+    This is the base class for TDK Lambda Genesys Series DC power supplies.
+
+    Do not directly instantiate an object with this class. Use one of the
+    TDK-Lambda power supply instrument classes that inherit from this parent
+    class.
     """
 
-    def __init__(self, adapter, address, name="TDK Lambda Base", **kwargs):
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Initializer and important communication methods
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def __init__(self, adapter, address, name="TDK-Lambda Base", **kwargs):
         super().__init__(
             adapter,
             name=name,
             includeSCPI=False,
             **kwargs
         )
-        self.adapter.connection.read_termination = '\r'
-        self.adapter.connection.write_termination = '\r'
+        self.adapter.connection.read_termination = "\r"
+        self.adapter.connection.write_termination = "\r"
         self.address = address
 
     def ask(self, command):
-        """ Queries a command to the instrument
+        """Queries a command to the instrument
         "OK\r"
 
         :param command: SCPI command string to be sent to the instrument
@@ -59,214 +79,266 @@ class TDK_Lambda_Base(Instrument):
         return self.adapter.connection.query(command)
 
     def write(self, command):
-        """ Queries a command to the instrument
+        """Writes a command to the instrument
         "OK\r"
 
         :param command: SCPI command string to be sent to the instrument
         """
         self.ask(command)
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Properties
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     address = Instrument.setting(
-        'ADR %d',
-        """ Address of the power supply [0-30] """,
+        "ADR %d",
+        """Set the address of the power supply [0-30].""",
         validator=strict_discrete_set,
         values=list(range(0, 31))
     )
 
     clear = Instrument.setting(
-        'CLS',
-        """Clear status sets FEVE and SEVE registers to zero """
+        "CLS",
+        """Set FEVE and SEVE registers to zero."""
     )
 
     reset = Instrument.setting(
-        'CLS',
-        """Clear status sets FEVE and SEVE registers to zero """
+        "RST",
+        """Set the instrument to restart."""
     )
 
     remote = Instrument.control(
-        'RMT?', 'RMT %d',
-        """Clear status sets FEVE and SEVE registers to zero """,
+        "RMT?", "RMT %d",
+        """Control remote operation of the power supply.
+        
+        Valid values are ``LOC`` for local mode, ``REM`` for remote mode, and 
+        ``LLC`` for local lockout mode.
+        """,
         validator=strict_discrete_set,
-        values=[0, 1, 2]
+        values={"LOC": 0, "REM": 1, "LLO": 2},
+        map_values=True
     )
 
     multidrop_capability = Instrument.measurement(
-        'MDAV?',
-        """Multi-drop option is available, 1 is installed, 0 is not installed """
+        "MDAV?",
+        """Measure to see if the multi-drop option is available on the power
+        supply. 
+        
+        If value is ``0``, the option is not available, if ``1`` it is 
+        available.
+        """
     )
 
     master_slave_setting = Instrument.measurement(
-        'MS?',
-        """Master and slave setting. Master: n = 1,2,3,4 Slave: n = 0 """
+        "MS?",
+        """Measure the master and slave settings. 
+        
+        Possible master values are ``1,2,3,4``. The slave value is ``0``.
+        """
     )
 
     repeat = Instrument.measurement(
-        '\\',
-        """ Repeat the last command """
+        "\\",
+        """Measure the last command again."""
     )
 
     identity = Instrument.measurement(
-        'IDN?',
-        """ Identity of the insturment """
+        "IDN?",
+        """Measure the identity of the instrument."""
     )
 
     version = Instrument.measurement(
-        'REV?',
-        """ Software version on instrument """
+        "REV?",
+        """Measure the software version on instrument."""
     )
 
     serial = Instrument.measurement(
-        'SN?',
-        """Serial number of the instrument """
+        "SN?",
+        """Measure the serial number of the instrument."""
     )
 
     last_test_date = Instrument.measurement(
-        'DATE?',
-        """ Date of the last experiment """
+        "DATE?",
+        """Measure the date of the last test."""
     )
 
     voltage = Instrument.control(
-        'PV?', 'PV %g',
-        """ Sets the desired output voltage """,
-        validator=lambda value, values: strict_discrete_range(value, values, step=.01),
+        "PV?", "PV %g",
+        """Control the programmed (set) output voltage.""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, step=0.01),
         values=[0, 40],
         dynamic=True
     )
 
     actual_voltage = Instrument.measurement(
-        'MV?',
-        """ Read the actual, measured voltage """
+        "MV?",
+        """Measure the the actual output voltage."""
     )
 
     current = Instrument.control(
-        'PC?', 'PC %g',
-        """ Sets the desired output current """,
-        validator=lambda value, values: strict_discrete_range(value, values, step=.01),
+        "PC?", "PC %g",
+        """Control the programmed (set) output current.""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, step=0.01),
         values=[0, 38],
         dynamic=True
     )
 
     actual_current = Instrument.measurement(
-        'MC?',
-        """ Read the actual, measured current """
+        "MC?",
+        """Measure the actual output current."""
     )
 
     mode = Instrument.measurement(
-        'MODE?',
-        """ Output mode of the power supply """
+        "MODE?",
+        """Measure the output mode of the power supply."""
     )
 
     display = Instrument.measurement(
-        'DVC?',
-        """ Read power supply data
-        Measured voltage, programmed voltage, measured current,
-        programmed current, over voltage set point, under voltage set point.
+        "DVC?",
+        """Measure the display voltage and current.
+        
+        Returns the measured voltage, programmed voltage, measured current,
+        programmed current, over voltage set point, and under voltage set point 
+        as a ``list`` containing ``float`` objects.
         """,
         get_process=lambda v: [float(i) for i in v.split(',')],
     )
 
     status = Instrument.measurement(
-        'STT?',
-        """ Power supply status
+        "STT?",
+        """Measure the power supply status.
+        
+        Returns a string of ASCII characters representing the actual voltage 
+        (MV), the programmed voltage (PV), the actual current (MC), the 
+        programmed current (PC), the status register (SR), and the fault
+        register (FR).        
         """
     )
 
     pass_filter = Instrument.control(
-        'FILTER?', 'FILTER %d',
-        """ Low pass filter frequency of the A to D converter
-        for voltage and current measurement
+        "FILTER?", "FILTER %d",
+        """Control the low pass filter frequency of the A to D converter
+        for voltage and current measurement.
+        
+        Valid frequency values ar 18, 23, or 46 Hz. Default value is 18 Hz.
         """,
         validator=strict_discrete_set,
         values=[18, 23, 46]
     )
 
     source_output = Instrument.control(
-        'OUT?', 'OUT %d',
-        """ Enable output of the power supply
+        "OUT?", "OUT %d",
+        """Control the output of the power supply.
+        
+        Valid states are ``ON`` and ``OFF``. 
         """,
         validator=strict_discrete_set,
-        values=[0, 1]
+        values={"ON": 1, "OFF": 0},
+        map_values=True
     )
 
     foldback = Instrument.control(
-        'FLD?', 'FLD %d',
-        """ Foldback protection status
+        "FLD?", "FLD %d",
+        """Control the fold back protection of the power supply.
+        
+        Valid states are ``ON`` to arm the fold back protection and ``OFF`` to 
+        cancels the fold back protection.
         """,
         validator=strict_discrete_set,
-        values=[0, 1]
+        values={"ON": 1, "OFF": 0},
+        map_values=True
     )
 
     foldback_delay = Instrument.control(
-        'FBD?', 'FBD %g',
-        """ Foldback delay that is multipled by 0.1 seconds
+        "FBD?", "FBD %g",
+        """Control the fold back delay.
+         
+        Adds an additional delay to the standard fold back delay (250 ms) by 
+        multiplying the set value by 0.1. Valid values are between 0 to 255
         """,
-        validator=lambda value, values: strict_discrete_range(value, values, step=.01),
-        values=[0, 255, 1]
+        validator=strict_range,
+        values=[0, 255],
+        cast=int
     )
 
     foldback_reset = Instrument.setting(
-        'FDBRST',
-        """ Foldback reset
+        "FDBRST",
+        """Set the additional fold back delay back to 0 s, restoring the
+        standard 250 ms delay.
         """
     )
 
     over_voltage = Instrument.control(
-        'OVP?', 'OVP %g',
-        """ Over voltage protection
-        """,
-        validator=lambda value, values: strict_discrete_range(value, values, step=.01),
+        "OVP?", "OVP %g",
+        """Control the over voltage protection.""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, step=0.01),
         values=[2, 44],
         dynamic=True
     )
 
     over_voltage_max = Instrument.setting(
-        'OVM',
-        """ Over voltage set to maximum
+        "OVM",
+        """Set the over voltage protection to the maximum level for the power
+        supply.
         """,
     )
 
     under_voltage = Instrument.control(
-        'UVL?', 'UVL %g',
-        """ Set under voltage limit
-        """,
-        validator=lambda value, values: strict_discrete_range(value, values, step=.01),
+        "UVL?", "UVL %g",
+        """Control the under voltage limit.""",
+        validator=lambda v, vs: strict_discrete_range(v, vs, step=0.01),
         values=[0, 38],
         dynamic=True
     )
 
     auto_restart = Instrument.control(
-        'AST?', 'AST %d',
-        """ Set auto restart mode
+        "AST?", "AST %d",
+        """Control the auto restart mode. 
+        
+        Valid values are ``ON`` and ``OFF``.
         """,
         validator=strict_discrete_set,
-        values=[0, 1]
+        values={"ON": 1, "OFF": 0},
+        map_values=True
     )
 
     save = Instrument.setting(
-        'SAV',
-        """ Save instrument settings
-        """,
+        "SAV",
+        """Set the instrument to save its settings.""",
     )
 
     recall = Instrument.setting(
-        'RCL',
-        """ Recall instrument settings
-        """,
+        "RCL",
+        """Set the instrument to recall its settings.""",
     )
 
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Methods
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     def ramp_to_current(self, target_current, steps=20, pause=0.2):
-        """ Ramps to a target current from the set current value over
+        """Ramps to a target current from the set current value over
         a certain number of linear steps, each separated by a pause duration.
-        :param target_current: A current in Amps
-        :param steps: An integer number of steps
-        :param pause: A pause duration in seconds to wait between steps """
-        currents = [round(i, 2) for i in np.linspace(self.current, target_current, steps)]
+
+        :param target_current: Target current in amps
+        :param steps: Integer number of steps
+        :param pause: Pause duration in seconds to wait between steps
+        """
+
+        currents = [round(i, 2) for i in np.linspace(self.current,
+                                                     target_current, steps)]
         for current in currents:
             self.current = current
             sleep(pause)
 
     def shutdown(self):
+        """Safety shutdown the power supply.
+
+        Ramps the power supply down to zero current using the
+        .ramp_to_current() method and turns the output off.
+        """
         log.info("Shutting down %s." % self.name)
         self.ramp_to_current(0.0)
-        self.source_output = False
+        self.source_output = "OFF"
+        log.info("%s has been shut down." % self.name)
         super().shutdown()
